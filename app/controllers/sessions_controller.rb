1bd5ad8ea6c10daf3a548e889cd8214092b3abf5
Elliot Waniek <elliot.waniek@kellogg.northwestern.edu>
2025-03-14 15:08:33 +0000

    fixed incorrect email/password crash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:email])
-    if BCrypt::Password.new(@user["password"]) == params["password"]
+    if @user && BCrypt::Password.new(@user["password"]) == params["password"]
       session[:user_id] = @user.id
       redirect_to places_path, notice: "Logged in successfully!"
     else
